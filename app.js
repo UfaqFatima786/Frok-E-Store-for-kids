fa45f117ffa7821d746e17904865a5d4e11e7ba1
UfaqFatima786 <u.capricorn19@gmail.com>
2026-05-20 12:27:51 +0500

    store

---
+++ app.js
@@ -0,0 +1,60 @@
+let products = document.getElementById("products");
+
+fetch("https://dummyjson.com/products")
+// fetch("https://fakestoreapi.com/products")
+
+.then((res) => {
+
+    return res.json();
+
+})
+// fetch("https://fakestoreapi.com/products")
+//   .then(res => {
+
+//     return res.json();
+
+// })
+
+.then((data) => {
+
+    console.log(data);
+
+    data.products.map((item) => {
+
+        products.innerHTML += `
+
+        <div class="card">
+
+            <img src="${item.thumbnail}" alt="" class="product-image">
+
+            <h3>${item.title}</h3>
+
+            <p>
+              ${item.description.substring(0, 80)}...
+            </p>
+
+            <div class="price">
+              $${item.price}
+            </div>
+
+            <button>Add To Cart</button>
+
+        </div>
+
+        `;
+    });
+
+})
+
+.catch((error) => {
+
+    console.log("API Error:", error);
+
+    products.innerHTML = `
+    
+      <h1>
+        Failed To Load Products
+      </h1>
+
+    `;
+});
