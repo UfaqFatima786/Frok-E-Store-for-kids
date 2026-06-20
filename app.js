ed06d940572c2575c82a9d1da850f85a6c3542d0
UfaqFatima786 <u.capricorn19@gmail.com>
2026-06-21 01:46:52 +0500

    general-mart

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 let products = document.getElementById("products");
-fetch("https://fakestoreapi.com/products/category/women's%20clothing")
-fetch("https://fakestoreapi.com/products")
+// fetch("https://fakestoreapi.com/products/category/women's%20clothing")
+// fetch("https://fakestoreapi.com/products")
 fetch("https://dummyjson.com/products")
-fetch("https://fakestoreapi.com/products/category/women's%20clothing")
+// fetch("https://fakestoreapi.com/products/category/women's%20clothing")
 
-.then((res) => {
+// .then((res) => {
 
-    return res.json();
+//     return res.json();
 
-})
+// })
+
 // fetch("https://fakestoreapi.com/products")
 //   .then(res => {
 
@@ -16,11 +17,21 @@ fetch("https://fakestoreapi.com/products/category/women's%20clothing")
 
 // })
 
-.then((data) => {
 
-    console.log(data);
 
-    data.products.map((item) => {
+
+// .then((data) => {
+
+//     console.log(data);
+
+//     data.products.map((item) => 
+      
+.then((res) => res.json())
+  .then((data) => {
+
+    data.products.map((item) =>
+
+      {
 
         products.innerHTML += `
 
@@ -60,3 +71,36 @@ fetch("https://fakestoreapi.com/products/category/women's%20clothing")
     `;
 });
 
+
+
+// let products = document.getElementById("products");
+
+// fetch("https://api.jsonbin.io/v3/b/6a36d816f5f4af5e29155231/latest", {
+//   headers: {
+//     "X-Master-Key": "$2a$10$eAUsi0jyaGySpBNB53Cg0et.i0Z9NBw4FhGy6DXWXzvp/mFrEPGfO"
+//   }
+// })
+// .then((res) => res.json())
+// .then((data) => {
+
+//   data.record.products.map((item) => {
+
+//     products.innerHTML += `
+//       <div class="card">
+//         <img src="${item.thumbnail}" alt="" class="product-image">
+
+//         <h3>${item.title}</h3>
+
+//         <p>${item.description}</p>
+
+//         <div class="price">$${item.price}</div>
+
+//         <button>Add To Cart</button>
+//       </div>
+//     `;
+//   });
+
+// })
+// .catch((err) => {
+//   console.log(err);
+// });
